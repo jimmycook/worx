3e80ccb0533c548edc0f3440503b6217d9b4d2e8
Jimmy <jimmy.cook.1993@gmail.com>
2018-01-25 19:55:29 +0000

    issues with windows, switching to mac

--- dist/41e2420436c4267a820b4b505c30d32f.js
+++ dist/41e2420436c4267a820b4b505c30d32f.js
@@ -69,7 +69,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({12:[function(require,module,exports) {
+})({24:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -100,7 +100,7 @@ function getBaseURL(url) {
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
 
-},{}],6:[function(require,module,exports) {
+},{}],9:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,17 +132,91 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":12}],5:[function(require,module,exports) {
+},{"./bundle-url":24}],6:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":6}],4:[function(require,module,exports) {
+},{"./..\\..\\node_modules\\normalize.css\\normalize.css":["normalize.css.css","normalize.css.js",10],"./..\\images\\bg.png":["ce8ee7bda27aac83955a3caf8cefdf4c.png",11],"./..\\images\\search.png":["37d6079154c90bc23a075681b93a8f4f.png",12],"./..\\images\\arrow_prev.png":["b2d4eca528cae5ec7a2c946f409b6e61.png",14],"./..\\images\\arrow_next.png":["771c5a8acca2be8d7f71321a7fadeb55.png",13],"_css_loader":9}],8:[function(require,module,exports) {
+"use strict";
+
+// Utility wrapper around query selectors
+function $(selector) {
+  var result = document.querySelectorAll(selector);
+  if (result) {
+    if (result.length > 1) return Array.from(result);
+    return result[0];
+  }
+}
+
+// Carousel code
+var prev = $('.prev');
+var next = $('.next');
+var items = $('.carousel__item');
+
+next.addEventListener('click', function () {
+  var active = $('.carousel__item.active');
+
+  var _itemIndexer = itemIndexer(items, active),
+      nextIndex = _itemIndexer.nextIndex;
+
+  var ne = items[nextIndex];
+  items.forEach(function (item) {
+    return item.classList.remove('active');
+  });
+  ne.classList.add('active');
+});
+
+prev.addEventListener('click', function () {
+  var active = $('.carousel__item.active');
+
+  var _itemIndexer2 = itemIndexer(items, active),
+      prevIndex = _itemIndexer2.prevIndex;
+
+  var ne = items[prevIndex];
+  items.forEach(function (item) {
+    return item.classList.remove('active');
+  });
+  ne.classList.add('active');
+});
+
+function itemIndexer(items, active) {
+  if (items) {
+    var activeIndex = items.indexOf(active);
+    return {
+      nextIndex: activeIndex + 1 >= items.length ? 0 : activeIndex + 1,
+      prevIndex: activeIndex - 1 < 0 ? items.length - 1 : activeIndex - 1
+    };
+  }
+}
+},{}],7:[function(require,module,exports) {
+"use strict";
+
+var map = void 0;
+
+var mapOptions = {
+  center: { lat: -34.397, lng: 150.644 },
+  zoom: 8
+};
+var elem = document.querySelector('.map');
+
+function initMap() {
+  if (elem) {
+    map = new google.maps.Map(elem, mapOptions);
+  }
+}
+
+window.initMap = initMap;
+},{}],5:[function(require,module,exports) {
 "use strict";
 
 require("../sass/app.scss");
-},{"../sass/app.scss":5}],0:[function(require,module,exports) {
+
+require("./carousel.js");
+
+require("./map.js");
+},{"../sass/app.scss":6,"./carousel.js":8,"./map.js":7}],0:[function(require,module,exports) {
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
 function Module() {
@@ -160,7 +234,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
-  var ws = new WebSocket('ws://' + window.location.hostname + ':60222/');
+  var ws = new WebSocket('ws://' + window.location.hostname + ':54777/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
@@ -261,4 +335,4 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id)
   });
 }
-},{}]},{},[0,4])
+},{}]},{},[0,5])
