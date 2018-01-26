366f4e72dd1be235080693d0256b132db13293f1
Jimmy Cook <jimmy@dun-pc-707.bigpartnership.lan>
2018-01-26 09:32:48 +0000

    map tweaks

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,14 +1,25 @@
 let map
 
-const mapOptions = {
-  center: { lat: -34.397, lng: 150.644 },
-  zoom: 8
+const whitespace = {
+  lat: 55.948950, 
+  lng: -3.205154
 }
+
+const mapOptions = {
+  center: whitespace,
+  disableDefaultUI: true,
+  zoom: 15  
+}
+
 const elem = document.querySelector('.map')
 
 function initMap() {
   if (elem) {
     map = new google.maps.Map(elem, mapOptions)
+    var marker = new google.maps.Marker({
+      position: whitespace,
+      map: map
+    })
   }
 }
 
--- dist/normalize.css.js
+++ dist/normalize.css.js
@@ -150,7 +150,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
-  var ws = new WebSocket('ws://' + window.location.hostname + ':58412/');
+  var ws = new WebSocket('ws://' + window.location.hostname + ':62865/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
